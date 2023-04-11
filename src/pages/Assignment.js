9dc9a186c46ef22783fe3abdbd64475abc96c39e
Shashwat Pandey <cool17shashwat@gmail.com>
2023-04-11 23:35:50 +0530

    Initial commit

---
+++ src/pages/Assignment.js
@@ -0,0 +1,159 @@
+import styles from "./Assignment.module.css";
+
+const Assignment = () => {
+  return (
+    <div className={styles.assignment}>
+      <img className={styles.bgIcon} alt="" src="/bg.svg" />
+      <div className={styles.assignmentChild} />
+      <div className={styles.assignmentItem} />
+      <div className={styles.assignmentInner} />
+      <div className={styles.rectangleDiv} />
+      <img className={styles.groupIcon} alt="" src="/group-7.svg" />
+      <div className={styles.thisIsAContainer}>
+        <span className={styles.thisIsA}>{`This is a `}</span>
+        <span className={styles.sample}>sample</span>
+      </div>
+      <div className={styles.loremIpsumDolor}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div className={styles.assignmentChild1} />
+      <div className={styles.searchForAnything}>Search for anything</div>
+      <div className={styles.assignmentChild2} />
+      <div className={styles.assignmentChild3} />
+      <div className={styles.enterEmailId}>Enter email ID</div>
+      <div className={styles.random}>Random</div>
+      <div className={styles.random1}>Random</div>
+      <div className={styles.login}>Login</div>
+      <div className={styles.registerNow}>Register now</div>
+      <div className={styles.imgLeft}>
+        <div className={styles.girl}>
+          <img className={styles.girlIcon} alt="" src="/girl@2x.png" />
+          <div className={styles.leftTextCurve}>
+            <div className={styles.copyCreateDesign}>C</div>
+            <div className={styles.copyCreateDesign1}>R</div>
+            <div className={styles.copyCreateDesign2}>E</div>
+            <div className={styles.copyCreateDesign3}>A</div>
+            <div className={styles.copyCreateDesign4}>T</div>
+            <div className={styles.copyCreateDesign5}>E</div>
+            <div className={styles.copyCreateDesign6}> </div>
+            <div className={styles.copyCreateDesign7}> </div>
+            <div className={styles.copyCreateDesign8}> </div>
+            <div className={styles.copyCreateDesign9}> </div>
+            <div className={styles.copyCreateDesign10}> </div>
+            <div className={styles.copyCreateDesign11}> </div>
+            <div className={styles.copyCreateDesign12}>D</div>
+            <div className={styles.copyCreateDesign13}>E</div>
+            <div className={styles.copyCreateDesign14}>S</div>
+            <div className={styles.copyCreateDesign15}>I</div>
+            <div className={styles.copyCreateDesign16}>G</div>
+            <div className={styles.copyCreateDesign17}>N</div>
+            <div className={styles.copyCreateDesign18}> </div>
+            <div className={styles.copyCreateDesign19}> </div>
+            <div className={styles.copyCreateDesign20}> </div>
+            <div className={styles.copyCreateDesign21}> </div>
+            <div className={styles.copyCreateDesign22}> </div>
+            <div className={styles.copyCreateDesign23}> </div>
+            <div className={styles.copyCreateDesign24}>I</div>
+            <div className={styles.copyCreateDesign25}>N</div>
+            <div className={styles.copyCreateDesign26}>N</div>
+            <div className={styles.copyCreateDesign27}>O</div>
+            <div className={styles.copyCreateDesign28}>V</div>
+            <div className={styles.copyCreateDesign29}>A</div>
+            <div className={styles.copyCreateDesign30}>T</div>
+            <div className={styles.copyCreateDesign31}>E</div>
+            <div className={styles.copyCreateDesign32}> </div>
+            <div className={styles.copyCreateDesign33}> </div>
+            <div className={styles.copyCreateDesign34}> </div>
+            <div className={styles.copyCreateDesign35}> </div>
+            <div className={styles.copyCreateDesign36}> </div>
+            <div className={styles.copyCreateDesign37}> </div>
+            <div className={styles.copyCreateDesign38}> </div>
+            <div className={styles.copyCreateDesign39}>I</div>
+            <div className={styles.copyCreateDesign40}>T</div>
+            <div className={styles.copyCreateDesign41}>E</div>
+            <div className={styles.copyCreateDesign42}>R</div>
+            <div className={styles.copyCreateDesign43}>A</div>
+            <div className={styles.copyCreateDesign44}>T</div>
+            <div className={styles.copyCreateDesign45}>E</div>
+          </div>
+        </div>
+        <div className={styles.leftText}>
+          <div className={styles.union}>
+            <div className={styles.unionChild} />
+            <div className={styles.unionItem} />
+          </div>
+          <div className={styles.goodDesignGoes}>
+            Good design goes to heaven
+          </div>
+        </div>
+        <div className={styles.designElement}>
+          <div className={styles.designElementChild} />
+          <div className={styles.designElementItem} />
+          <div className={styles.designElementInner} />
+        </div>
+      </div>
+      <div className={styles.imgRight}>
+        <div className={styles.man}>
+          <img className={styles.manIcon} alt="" src="/man@2x.png" />
+          <div className={styles.rightTextCurve}>
+            <div className={styles.copyCreateDesign46}>C</div>
+            <div className={styles.copyCreateDesign47}>O</div>
+            <div className={styles.copyCreateDesign48}>D</div>
+            <div className={styles.copyCreateDesign49}>E</div>
+            <div className={styles.copyCreateDesign50}> </div>
+            <div className={styles.copyCreateDesign51}> </div>
+            <div className={styles.copyCreateDesign52}> </div>
+            <div className={styles.copyCreateDesign53}> </div>
+            <div className={styles.copyCreateDesign54}> </div>
+            <div className={styles.copyCreateDesign55}> </div>
+            <div className={styles.copyCreateDesign56}>C</div>
+            <div className={styles.copyCreateDesign57}>O</div>
+            <div className={styles.copyCreateDesign58}>D</div>
+            <div className={styles.copyCreateDesign59}>E</div>
+            <div className={styles.copyCreateDesign60}> </div>
+            <div className={styles.copyCreateDesign61}> </div>
+            <div className={styles.copyCreateDesign62}> </div>
+            <div className={styles.copyCreateDesign63}> </div>
+            <div className={styles.copyCreateDesign64}> </div>
+            <div className={styles.copyCreateDesign65}> </div>
+            <div className={styles.copyCreateDesign66}>C</div>
+            <div className={styles.copyCreateDesign67}>O</div>
+            <div className={styles.copyCreateDesign68}>D</div>
+            <div className={styles.copyCreateDesign69}>E</div>
+            <div className={styles.copyCreateDesign70}> </div>
+            <div className={styles.copyCreateDesign71}> </div>
+            <div className={styles.copyCreateDesign72}> </div>
+            <div className={styles.copyCreateDesign73}> </div>
+            <div className={styles.copyCreateDesign74}> </div>
+            <div className={styles.copyCreateDesign75}> </div>
+            <div className={styles.copyCreateDesign76}>D</div>
+            <div className={styles.copyCreateDesign77}>E</div>
+            <div className={styles.copyCreateDesign78}>B</div>
+          </div>
+        </div>
+        <div className={styles.rightText}>
+          <div className={styles.rightTextChild} />
+          <div className={styles.itsNotAContainer}>
+            <p className={styles.itsNotA}>{`Its not a bug, `}</p>
+            <p className={styles.itsNotA}>its a feature</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.image3Wrapper}>
+        <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+      </div>
+      <div className={styles.image2Wrapper}>
+        <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      </div>
+      <div className={styles.image5Wrapper}>
+        <img className={styles.image5Icon} alt="" src="/image-5@2x.png" />
+      </div>
+      <div className={styles.image4Wrapper}>
+        <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+      </div>
+      <img className={styles.image6Icon} alt="" src="/image-6@2x.png" />
+    </div>
+  );
+};
+
+export default Assignment;
